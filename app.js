c6096d3512902035764c5c8a957ee41ccb796cd6
Ihamria <hamriaimane69@gmail.com>
2020-05-04 09:20:41 +0000

    Description du commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser= require('body-parser')
 const express = require('express');
 const fs = require('fs')
 const app = express();
-const port = 3000;
+const port = 1337;
 
 const read = fs.readFileSync('./data/Marque.json');
 let data= JSON.parse(read);
@@ -20,13 +20,28 @@ app.use(express.static(__dirname + '/views'));
 app.get('/', (req, res) => {
  res.render('pages/index',{data});
 });
+//add
+app.get('/Marque',(req,res)=>{
+	res.render('Marque', {data})
+});
 
-
-app.post('/add', (req, res) => {
-	const { Marque,Collection,Categorie} = req.body;
-
-	data.push({Marque:Marque, Collection:Collection, Categorie:Categorie});
-	fs.writeFileSync('./data/Marque.json', JSON.stringify(data, null, 4));
+app.post('/Marque', function(req, res){
+	if(req.body.Marque==""||req.body.Collection==""||req.body.Categorie=="" || req.body.ID==""){
+		res.redirect('/Marque')
+	}
+else{
+	for(var i in data){
+		if(data[i].ID==req.body.ID){
+		data.push({
+			"Marque":req.body.Marque,
+			"Collectin":req.body.Collection,
+			"Categorie":req.body.Categorie,
+			"ID":req.body.ID
+		});
+		fs.writeFileSync('./data/Marque.json', JSON.stringify(data, null, 4));
 	res.render('Marque.ejs');
+	}
+}
+}
 });
 app.listen(port, () => console.log(`localhost ${port}!`));
